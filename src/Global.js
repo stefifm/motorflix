dc49643ba3cdea5a2fe60ce25a18c3156e120c88
stefifm <bruerastefania@gmail.com>
2023-04-28 13:55:58 -0300

    Create global styles

---
+++ src/Global.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+import {
+  colorBlack,
+  colorRedDark,
+  fontWeightLight,
+  fontWeightNormal,
+} from './components/UI/variablesStyle'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Roboto', sans-serif;
+    background: -webkit-linear-gradient(145deg, ${colorRedDark} 20%, ${colorBlack} 70%);
+  }
+
+  h1 {
+    font-size: 3.75rem;
+    font-weight: ${fontWeightNormal};
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    font-weight: ${fontWeightNormal};
+  }
+
+  h3 {
+    font-size: 1.875rem;
+    font-weight: ${fontWeightNormal};
+  }
+
+  h4 {
+    font-size: 1.688rem;
+    font-weight: ${fontWeightLight};
+  }
+
+  p {
+    font-size: 1.125rem;
+    font-weight: ${fontWeightLight};
+  }
+
+  span {
+    font-size: 1rem;
+    font-weight: ${fontWeightLight};
+  }
+`
