f1e1a396e0b64307813ae83910008b59062b14ea
stefifm <bruerastefania@gmail.com>
2023-04-28 13:55:48 -0300

    Create variables for styles

---
+++ src/components/UI/variablesStyle.js
@@ -0,0 +1,21 @@
+export const colorPrimary = '#2A7AE4'
+
+export const colorWec = '#6BD1FF'
+export const colorGt = '#00C86F'
+export const colorIndyCar = '#6B5BE2'
+export const colorElms = '#FFBA05'
+
+export const colorBlack = '#000000'
+export const colorBlackMedium = '#4A4A4A'
+export const colorBlackLight = '#5C5C5C'
+export const colorBlackLighter = '#9E9E9E'
+export const colorWhite = '#F5F5F5'
+
+export const colorGray = '#C2C2C2'
+export const colorGrayMedium = '#E5E5E5'
+
+export const colorRed = '#C62828'
+export const colorRedDark = '#BE3144'
+
+export const fontWeightNormal = '400'
+export const fontWeightLight = '300'
