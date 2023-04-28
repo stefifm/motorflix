983e2152a1d6fca6a074b54abe43dbb1e45a2db7
stefifm <bruerastefania@gmail.com>
2023-04-28 13:56:48 -0300

    Banner component

---
+++ src/components/common/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import { Card, CardMedia } from '@mui/material'
+import banner from '../../../assets/img/banner.png'
+import { styled } from '@mui/material/styles'
+
+const BannerBox = styled(Card)(({ theme }) => ({
+  marginTop: '4.8rem',
+}))
+
+const BannerImg = styled(CardMedia)(({ theme }) => ({
+  width: '100%',
+  objectFit: 'cover',
+}))
+
+function Banner() {
+  return (
+    <BannerBox>
+      <BannerImg
+        component='img'
+        image={banner}
+        alt='banner'
+      />
+    </BannerBox>
+  )
+}
+
+export default Banner
