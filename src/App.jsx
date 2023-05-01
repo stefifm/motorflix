9349f37bd03a130100a788dce82b97e8f964a79e
stefifm <bruerastefania@gmail.com>
2023-04-30 22:25:53 -0300

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { GlobalStyle } from './Global'
 import DefaultPage from './components/pages/DefaultPage/DefaultPage'
 import Home from './components/pages/Home/Home'
+import FormPages from './components/pages/FormPages/FormPages'
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <DefaultPage>
-        <Home />
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/formularios'
+            element={<FormPages />}
+          />
+        </Routes>
       </DefaultPage>
-    </>
+    </Router>
   )
 }
 
