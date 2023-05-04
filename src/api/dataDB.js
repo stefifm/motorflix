94e2dba1babac9c3c1f68eaa3b72a99577f5d5bf
stefifm <bruerastefania@gmail.com>
2023-05-03 23:33:09 -0300

    Change base url

--- src/api/dataDB.js
+++ src/api/dataDB.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const dataDB = axios.create({
-  baseURL: 'https://motorflix-api.onrender.com/'
+  baseURL: 'https://motorflix-api.azurewebsites.net/'
 })
 
 export const getVideos = () => dataDB.get('/videos')
